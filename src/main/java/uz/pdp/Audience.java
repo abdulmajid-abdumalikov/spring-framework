87b27c8d9668710cba3ecf9d88abff2f44be34f7
abdulmajid <abdulmajid.malikov3@gmail.com>
2024-06-04 00:49:52 +0500

    aop-xml-config done

--- src/main/java/uz/pdp/Audience.java
+++ src/main/java/uz/pdp/Audience.java
@@ -1,20 +1,26 @@
 package uz.pdp;
 
+import org.aspectj.lang.annotation.*;
+
+@Aspect
 public class Audience {
 
-
+    @Before("execution(* uz.pdp.Performance.perform(..))")
     public void silencingPhones() {
         System.out.println("Silencing cell phones");
     }
 
+    @Before("execution(* uz.pdp.Performance.perform(..))")
     public void takeSeats() {
         System.out.println("Taking Seats");
     }
 
+    @AfterReturning("execution(* uz.pdp.Performance.perform(..))")
     public void applause() {
         System.out.println("👏👏👏");
     }
 
+    @AfterThrowing("execution(* uz.pdp.Performance.perform(..))")
     public void refund(){
         System.out.println("🤬🤬🤬");
     }
--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -1,10 +1,11 @@
 package uz.pdp;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("ioc-config.xml");
+        AnnotationConfigApplicationContext container = new AnnotationConfigApplicationContext(AppConfig.class);
         Performance performance = container.getBean(Performance.class);
         performance.perform();
     }
