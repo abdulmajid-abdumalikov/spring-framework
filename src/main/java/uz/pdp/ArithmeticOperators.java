b5c4f3e1af6911b786bdfcbb063e13333b00adde
abdulmajid <abdulmajid.malikov3@gmail.com>
2024-06-03 23:51:37 +0500

    commit

--- src/main/java/uz/pdp/ArithmeticOperators.java
+++ src/main/java/uz/pdp/ArithmeticOperators.java
@@ -1,12 +1,26 @@
 package uz.pdp;
 
 import lombok.ToString;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @ToString
 public class ArithmeticOperators {
 
+    @Value("#{1+2}")
     public int add;
+
+    @Value("#{12 ^ 2}")
+    public int pow;
+
+
+    @Value("#{12 mod 5}")
+    public int mod;
+
+    @Value("#{12 gt 5}")
+    public boolean gt;
+
+    @Value("#{12 < 5 ?'tvelve':'five'}")
+    public String ternary;
 }
--- src/main/java/uz/pdp/Main2.java
+++ src/main/java/uz/pdp/Main2.java
@@ -6,5 +6,9 @@ public class Main2 {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("uz.pdp");
         System.out.println(context.getBean(ArithmeticOperators.class).add);
+        System.out.println(context.getBean(ArithmeticOperators.class).pow);
+        System.out.println(context.getBean(ArithmeticOperators.class).mod);
+        System.out.println(context.getBean(ArithmeticOperators.class).gt);
+        System.out.println(context.getBean(ArithmeticOperators.class).ternary);
     }
 }
